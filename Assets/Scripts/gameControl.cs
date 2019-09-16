aa3a056cff4be88accff6d7d8c3d03adba5b7f30
zcs18 <zcs18@hampshire.edu>
2019-09-16 01:30:20 -0400

    Part 1 is done

---
+++ Assets/Scripts/gameControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameControl : MonoBehaviour
+{
+    int printTest = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Time.time > 2.99 && printTest == 0)
+        {
+            print("It's been three seconds!");
+            printTest = 1;
+        }
+
+    }
+}
